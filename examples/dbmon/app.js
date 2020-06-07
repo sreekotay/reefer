52b4dc34cfe51507b1972b0d6531916759fa17d2
Sree Kotay <sreekotay@gmail.com>
2020-06-06 22:35:43 -0400

    margin and padding for sticky

--- examples/dbmon/app.js
+++ examples/dbmon/app.js
@@ -1,10 +1,15 @@
 
+perfMonitor.startFPSMonitor()
+perfMonitor.startMemMonitor()
+perfMonitor.initProfiler("render")
+
 var anim = true
 var render = function () {
-  Monitoring.renderRate.ping()
   databases = ENV.generateData(true).toArray()
+	perfMonitor.startProfile("render")
   var reef = reefer.find('#reeftable')
   reef.render()
+	perfMonitor.endProfile("render")
   if (anim) setTimeout(render, ENV.timeout)
 }
 
